2fb451f7c935deb8b7284aec4599f35a6642682c
lai2-01 <lai2-01@itismeucci.com>
2021-11-29 11:42:45 +0100

    Second Commit

--- src/main/java/com/cangelosi/App.java
+++ src/main/java/com/cangelosi/App.java
@@ -1,13 +1,11 @@
 package com.cangelosi;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Server server = new Server();
+        server.attendi();
+        server.comunica();
     }
 }
